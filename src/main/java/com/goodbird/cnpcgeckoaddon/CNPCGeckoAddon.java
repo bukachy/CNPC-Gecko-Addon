95d239c1a64a281bbdb569389e2e7879bc12fe86
MTechLab Dev <dliapromua@gmail.com>
2025-02-04 21:19:23 +0200

    Update CNPCGeckoAddon.java

--- src/main/java/com/goodbird/cnpcgeckoaddon/CNPCGeckoAddon.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/CNPCGeckoAddon.java
@@ -1,8 +1,11 @@
 package com.goodbird.cnpcgeckoaddon;
 
 import com.goodbird.cnpcgeckoaddon.network.NetworkWrapper;
+import com.goodbird.cnpcgeckoaddon.registry.EntityRegistry;
 import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
@@ -12,8 +15,9 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 public class CNPCGeckoAddon {
     public static final String MODID = "cnpcgeckoaddon";
 
-    public CNPCGeckoAddon() {
+    public CNPCGeckoAddon(IEventBus modEventBus, ModContainer modContainer) {
         NeoForge.EVENT_BUS.register(this);
+        EntityRegistry.ENTITIES.register(modEventBus);
     }
 
     @SubscribeEvent
