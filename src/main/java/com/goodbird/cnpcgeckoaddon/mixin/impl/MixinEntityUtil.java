15523b2b793036e3427cc836537b13a397441a8d
MTechLab Dev <dliapromua@gmail.com>
2025-02-04 18:37:48 +0200

    Update MixinEntityUtil.java

--- src/main/java/com/goodbird/cnpcgeckoaddon/mixin/impl/MixinEntityUtil.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/mixin/impl/MixinEntityUtil.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
 
 @Mixin(EntityUtil.class)
 public class MixinEntityUtil {
