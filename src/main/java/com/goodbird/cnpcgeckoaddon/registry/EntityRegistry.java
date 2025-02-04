685ff841dc266cc7fa6081edfd4cd3c938af6e48
MTechLab Dev <dliapromua@gmail.com>
2025-02-04 21:17:18 +0200

    Update EntityRegistry.java

--- src/main/java/com/goodbird/cnpcgeckoaddon/registry/EntityRegistry.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/registry/EntityRegistry.java
@@ -2,10 +2,7 @@ package com.goodbird.cnpcgeckoaddon.registry;
 
 import com.goodbird.cnpcgeckoaddon.CNPCGeckoAddon;
 import com.goodbird.cnpcgeckoaddon.entity.EntityCustomModel;
-import com.goodbird.cnpcgeckoaddon.utils.ObjectHolder;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
@@ -13,35 +10,28 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.registries.RegisterEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = CNPCGeckoAddon.MODID)
 public class EntityRegistry {
 
-    @ObjectHolder(registryName ="entity_type", value = CNPCGeckoAddon.MODID+":custommodelentity")
-    public static EntityType<? extends EntityCustomModel> entityCustomModel;
 
-    @SubscribeEvent
-    public static void registerEntities(RegisterEvent event) {
-        if (event.getRegistryKey() == BuiltInRegistries.ENTITY_TYPE) {
-            registerNewentity(event.getRegistry(), EntityCustomModel.class, "custommodelentity", EntityCustomModel::new, 64, 10, false, 0.7F, 2F);
-        }
-    }
+    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, CNPCGeckoAddon.MODID);
 
-    private static <T extends Entity> void registerNewentity(final IForgeRegistry<EntityType<?>> registry, final Class<? extends Entity> c, final String name, final EntityType.EntityFactory<T> factoryIn, final int range, final int update, final boolean velocity, final float width, final float height) {
-        final EntityType.Builder<?> builder = EntityType.Builder.of(factoryIn, MobCategory.MISC);
-        builder.setTrackingRange(range);
-        builder.setUpdateInterval(update);
-        builder.setShouldReceiveVelocityUpdates(velocity);
-        builder.sized(width, height);
-        builder.clientTrackingRange(4);
-        final ResourceLocation registryName = ResourceLocation.fromNamespaceAndPath(CNPCGeckoAddon.MODID, name);
-        registry.register(registryName, builder.build(registryName.toString()));
-    }
+    public static final DeferredHolder<EntityType<?>, EntityType<EntityCustomModel>> CUSTOM_MODEL_ENTITY = ENTITIES.register("custommodelentity",
+            () -> EntityType.Builder.of(EntityCustomModel::new, MobCategory.MISC)
+                    .sized(0.7F, 2F)
+                    .setTrackingRange(64)
+                    .setUpdateInterval(10)
+                    .setShouldReceiveVelocityUpdates(false)
+                    .clientTrackingRange(4)
+                    .build("custommodelentity")
+    );
 
 
     @SubscribeEvent
     public static void attribute(final EntityAttributeCreationEvent event) {
-        event.put(entityCustomModel, LivingEntity.createLivingAttributes().add(Attributes.FOLLOW_RANGE).build());
+        event.put(CUSTOM_MODEL_ENTITY.get(), LivingEntity.createLivingAttributes().add(Attributes.FOLLOW_RANGE).build());
     }
 }
