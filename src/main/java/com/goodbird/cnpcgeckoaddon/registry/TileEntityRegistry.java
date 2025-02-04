1bf1da8a1a6732aecc7de5e80ff95b89d324ff7d
bukachy <santikso1337@gmail.com>
2025-02-04 23:50:57 +0700

    Update TileEntityRegistry.java

--- src/main/java/com/goodbird/cnpcgeckoaddon/registry/TileEntityRegistry.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/registry/TileEntityRegistry.java
@@ -3,31 +3,33 @@ package com.goodbird.cnpcgeckoaddon.registry;
 import com.goodbird.cnpcgeckoaddon.CNPCGeckoAddon;
 import com.goodbird.cnpcgeckoaddon.tile.TileEntityCustomModel;
 import net.minecraft.Util;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.registries.RegisterEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = CNPCGeckoAddon.MODID)
+import java.util.function.Supplier;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = CNPCGeckoAddon.MODID)
 public class TileEntityRegistry {
+//    @ObjectHolder(registryName ="block_entity_type", value = CNPCGeckoAddon.MODID+":custommodeltileentity")
+//    public static BlockEntityType<? extends TileEntityCustomModel> tileEntityCustomModel;
+//
+//    @SubscribeEvent
+//    public static void registerBlocks(RegisterEvent event) {
+//        if (event.getRegistryKey() == BuiltInRegistries.BLOCK_ENTITY_TYPE) {
+//            event.getRegistry().registr(CNPCGeckoAddon.MODID+":custommodeltileentity", createTile("custommodeltileentity",TileEntityCustomModel::new));
+//        }
+//    }
 
-    @ObjectHolder(registryName ="block_entity_type", value = CNPCGeckoAddon.MODID+":custommodeltileentity")
-    public static BlockEntityType<? extends TileEntityCustomModel> tileEntityCustomModel;
-
-    @SubscribeEvent
-    public static void registerBlocks(RegisterEvent event) {
-        if (event.getRegistryKey() == ForgeRegistries.Keys.BLOCK_ENTITY_TYPES) {
-            event.getForgeRegistry().register(CNPCGeckoAddon.MODID+":custommodeltileentity", createTile("custommodeltileentity",TileEntityCustomModel::new));
-        }
-    }
-
-    private static BlockEntityType<?> createTile(String key, BlockEntityType.BlockEntitySupplier factoryIn, Block... blocks){
-        BlockEntityType.Builder<BlockEntity> builder = BlockEntityType.Builder.of(factoryIn, blocks);
-        return builder.build(Util.fetchChoiceType(References.BLOCK_ENTITY, key));
-    }
+//    private static BlockEntityType<?> createTile(String key, BlockEntityType.BlockEntitySupplier factoryIn, Block... blocks){
+//        BlockEntityType.Builder<BlockEntity> builder = BlockEntityType.Builder.of(factoryIn, blocks);
+//        return builder.build(Util.fetchChoiceType(References.BLOCK_ENTITY, key));
+//    }
 }
